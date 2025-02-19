5eba0ffabd4b97f4915542c5bdd4004ac89acb72
Bhargav-devops <bhargav.devopsaws@gmail.com>
2025-02-19 20:11:45 +0530

    roboshop shared library

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -7,7 +7,7 @@ def call(Map configMap){
         }
         environment { 
             packageVersion = ''
-            nexusURL = '172.31.5.95:8081'
+            nexusURL = '172.31.45.24:8081'
         }
         options {
             timeout(time: 1, unit: 'HOURS')
